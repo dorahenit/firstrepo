a81c032e8c95d5323304e451274a699907036395
DORA HENIT <DORA HENIT@DORAHENIT>
2023-09-15 17:00:01 +0530

    myfourthproject

---
+++ mymavenproject1/src/main/java/encapsulation1/Encapclass2.java
@@ -0,0 +1,14 @@
+package encapsulation1;
+
+public class Encapclass2 {
+
+	public static void main(String[] args) {
+		Encapclass1 a=new Encapclass1();
+		a.setName("Dora");
+		a.setSalary(1000);
+		System.out.println(a.getName());
+		System.out.println(a.getSalary());
+
+	}
+
+}
